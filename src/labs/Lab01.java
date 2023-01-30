f241f82ea34024356d6987da1c8a521d2e669e69
AdmiralChimp <gjohnson@newtech.coppellisd.com>
2023-01-30 10:37:16 -0600

    Unit 5

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -18,6 +18,8 @@ public class Lab01 {
 	
 	
 	public static void problem1() {
+		
+		System.out.println("3^5 = " + Math.pow(3, 5));
 	
 		
 	}
@@ -26,21 +28,49 @@ public class Lab01 {
 	
 	public static void problem2() {
 		
-			
+		System.out.println("sqare root of 10 = " + Math.sqrt(10));	
+		
 	}
 	
 	
 	
 	public static void problem3() {
 		
+		int a = 25, b = 49;
+		System.out.println("(a1 + a3) = " + (a + b) + "\n");
 		
 	}
 	
 	
 	
 	public static void problem4() {
+		Scanner inKey = new Scanner(System.in); 
+		int entnum; 
+		int max = Integer.MIN_VALUE; 
+		int min = Integer.MAX_VALUE; 
 		
-		
+		System.out.print("Enter an Integer");
+		entnum = inKey.nextInt();
+		while (entnum > 0)
+			{
+			System.out.print("Enter an integer");
+			entnum = inKey.nextInt(); 
+			if (entnum < 0)
+		{
+			System.out.println("Enter an integer");	
+			entnum = inKey.nextInt(); 
+			max = Math.max(max, entnum); 
+			
+			System.out.println(max);
+			System.out.println("max =" + max);
+			min = Math.min(min, entnum); 
+			
+			System.out.println(min); 
+			System.out.println("Min=" + min);
+				
+				
+				
+			}
 	}
 	
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -15,13 +15,36 @@ public class Lab02 {
 
 
 	public static void problem1() {
-		
+		Scanner inKey = new Scanner(System.in); 
+		double entnum; 
+		double entnum2; 
+		System.out.println("Enter a positive integer"); 
+		entnum = inKey.nextInt();
+		System.out.println("Enter another positive integer"); 
+		entnum2 = inKey.nextInt(); 
+		for (int i = 1; i < 11; i++) {
+		double scope = entnum;
+		double min = entnum2; 
+			int num = (int) Math.random() scope + min; 
+			System.out.println(num); 
 		
 	}
 
 
 	public static void problem2() {
 		
+	Scanner inKey = new Scanner(System.in); 
+	double Radius;
+	double Height;
+	double PI; 
+	PI  = 3.14
+	System.out.println("Enter the radius"); 
+	Radius = inKey.nextInt();
+	System.out.println("Enter the height"); 
+	Height = inKey.nextInt(); 
+	
+			
+	
 		
 	}
 
